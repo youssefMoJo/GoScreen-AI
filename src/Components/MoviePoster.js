d3f8b4dbf790cae7f6a38a39554b720f6df3d646
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2023-11-19 18:47:10 -0330

    Completed the animation for the movie posters.

--- src/Components/MoviePoster.js
+++ src/Components/MoviePoster.js
@@ -1,17 +1,35 @@
+import React, { useEffect, useState } from "react";
 import poster from "../assets/poster.jpg";
 
 const MoviePoster = () => {
-  const moviePoterStyle = {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    const animateCard = async () => {
+      await delay(400);
+
+      setIsLoaded(true);
+    };
+
+    animateCard();
+  }, []);
+
+  const moviePosterStyle = {
     height: "21rem",
     borderRadius: "4%",
     position: "absolute",
     top: "35%",
     left: "10%",
+    opacity: isLoaded ? 1 : 0,
+    transform: isLoaded ? "scale(1) rotate(0deg)" : "scale(0.8) rotate(-15deg)",
+    transition: "opacity 0.4s ease-in-out, transform 0.4s ease-in-out",
   };
 
   return (
     <div>
-      <img src={poster} style={moviePoterStyle} />
+      <img src={poster} style={moviePosterStyle} alt="Movie Poster" />
     </div>
   );
 };
