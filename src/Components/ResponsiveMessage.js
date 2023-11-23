8282e0adf6f9a647f4e494e4f996c3c5b7d0f89c
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2023-11-23 14:49:41 -0330

    add the ResponsiveMessageAnimation

--- src/Components/ResponsiveMessage.js
+++ src/Components/ResponsiveMessage.js
@@ -1,3 +1,6 @@
+import Lottie from "lottie-react";
+import ResponsiveMessageAnimation from "../assets/ResponsiveMessageAnimation.json";
+
 const ResponsiveMessage = () => {
   const responsiveMessageStyle = {
     position: "absolute",
@@ -11,6 +14,11 @@ const ResponsiveMessage = () => {
     border: "1px solid #f5c6cb",
   };
 
+  const responsiveMessageAnimationStyle = {
+    width: "70%",
+    margin: "auto",
+  };
+
   return (
     <div style={responsiveMessageStyle}>
       <p>
@@ -22,6 +30,11 @@ const ResponsiveMessage = () => {
         free to explore, but for the best user experience, we recommend using a
         device with a larger screen. Thank you for your understanding!
       </p>
+      <Lottie
+        style={responsiveMessageAnimationStyle}
+        loop={true}
+        animationData={ResponsiveMessageAnimation}
+      />
     </div>
   );
 };
